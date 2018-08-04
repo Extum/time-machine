7d87cd9b22b9f2f8d7e9edcf13ae8abb22c7d7bc
Arda Çebi <ardacebi1@gmail.com>
2018-08-04 23:29:15 +0300

    Commit

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@
  *
  *
  *  For the full copyright and license information, please view the LICENSE.md
- *  file that was distributed with this source code.
+ *  file that was distributed with this source code. 
  */
 namespace Extum\TimeMachine;
 use Illuminate\Contracts\Events\Dispatcher;
